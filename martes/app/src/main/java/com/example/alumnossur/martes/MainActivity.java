0285a78e632a4b54bba349c9945de45d66e0c081
alumnosssur <alumnossur@inacap.cl>
2018-01-09 09:38:47 -0300

    modificacion estructura linea base. una carpeta por dia. por ahora, lunes y martes

---
+++ martes/app/src/main/java/com/example/alumnossur/martes/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.alumnossur.martes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lanzaInvitacion(View view) {
+    }
+
+    public void lanzaOtraActividad(View view) {
+    }
+
+    public void lanzaActividadCafe(View view) {
+    }
+
+    public void lanzaActividadTe(View view) {
+    }
+}
